5dea841c5f6ca78744819804502d5e1b01156c00
vijayaramabishek <43339227+vijayaramabishek@users.noreply.github.com>
2018-10-01 11:55:20 +0530

    Python File

--- Movie.py
+++ Movie.py
@@ -80,4 +80,27 @@ for x in xrange(0, xs):
     r /= 255.0
     g /= 255.0
     b /= 255.0
+    def open_image(path):
+  newImage = Image.open(path)
+  return newImage
+
+# Save Image
+def save_image(image, path):
+  image.save(path, 'png')
+
+# Create a new image with the given size
+def create_image(i, j):
+  image = Image.new("RGB", (i, j), "white")
+  return image
+
+# Get the pixel from the given image
+def get_pixel(image, i, j):
+    # Inside image bounds?
+    width, height = image.size
+    if i > width or j > height:
+      return None
+
+    # Get Pixel
+    pixel = image.getpixel((i, j))
+    return pixel
 print(img)
